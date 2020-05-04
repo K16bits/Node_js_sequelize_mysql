852d0db2a55a55e0193687e866c5d04df696b026
K16bits <52179531+K16bits@users.noreply.github.com>
2020-05-04 18:28:52 -0300

    Update ListarDadosController.js

--- src/data/controllers/ListarDadosController.js
+++ src/data/controllers/ListarDadosController.js
@@ -4,8 +4,7 @@ const Pessoa = require("../models/Pessoa");
 module.exports = {
     async Listar(req,res){
         // Find all users
-        const x = await Pessoa.findAll({attributes:['nome','senha']});  //THIS IS IMPORTE CARAI ({raw:true})
-        
+        const x = await Pessoa.findAll({attributes:['nome','senha']});  //THIS IS important CARAI ({raw:true})     
         res.json(x);   
     }
 }
