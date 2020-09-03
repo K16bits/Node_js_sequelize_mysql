be1a8ed772eb384a0786133a16ebad6fd2c25e9e
K16bits <52179531+K16bits@users.noreply.github.com>
2020-09-03 15:45:45 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,14 @@ const routes = express.Router();
 
 const AdicionarController = require("./data/controllers/AdicionarController");
 
-const ListarDados = require("./data/controllers/ListarDadosController.js"); //teste
+const ListarDados = require("./data/controllers/ListarDadosController.js"); 
 const Pessoa = require("./data/models/Pessoa")
 
 routes.get("/",(req,res)=>{
     res.json({"hello":"world"});
 })
 
-routes.post("/add",AdicionarController.store); //Test
+routes.post("/add",AdicionarController.store); 
 
 routes.get("/see",ListarDados.Listar);
 
