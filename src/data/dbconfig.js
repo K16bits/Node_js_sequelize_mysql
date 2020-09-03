f51f3004f595f92803103a6bdebbf16cec01dd2e
K16bits <52179531+K16bits@users.noreply.github.com>
2020-09-03 16:02:21 -0300

    Update dbconfig.js

--- src/data/dbconfig.js
+++ src/data/dbconfig.js
@@ -2,5 +2,6 @@ module.exports = {
     "username":"root",
     "password":"",
     "host":"localhost",
+    "dialect":"mysql",
     "database":"test" //Nome do Banco
 } //Configurações para se conectar ao XAMPP
